7dd628bd7e9114499edf7f5ca3cde32fe87ba73b
Danyc04 <cerquerasaezdaniela@gmail.com>
2024-04-14 17:01:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,17 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+<<<<<<< Updated upstream
 from Frontend.frontend import layout
 
 app.layout = layout
+=======
+
+variable_1_cen = dbc.Container([
+    html.H1("Texto 1 cen")
+])
+#app.layout = layout
+>>>>>>> Stashed changes
 
 if __name__ =='__main__':
     app.run_server(debug=True)
