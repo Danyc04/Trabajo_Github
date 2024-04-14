5b9204446c0126ac8a98f9a329e290825a33726b
JohanPe65 <163649619+JohanPe65@users.noreply.github.com>
2024-04-14 15:51:09 -0500

    a

--- Frontend/frontend.py
+++ Frontend/frontend.py
@@ -2,12 +2,17 @@ import dash
 from dash import html 
 import dash_bootstrap_components as dbc  
 
+from Estructuras.Central import Central
+from Estructuras.Navegador import Navegador
+from Estructuras.Inferior_der import Derecha
+from Estructuras.Inferior_izq import Izquierda
+
 layout = dbc.Container( [
     dbc.Row([
-        dbc.Col('navegador', md=12,style={'background-color': 'orange'}),
-        dbc.Col('central', md=12,style={'background-color': 'white'}),
-        dbc.Col('inferior izq', md=6,style={'background-color': 'blue'}),
-        dbc.Col('inferior der', md=6,style={'background-color': 'red'})
+        dbc.Col(Navegador, md=12,style={'background-color': 'orange'}),
+        dbc.Col(Central, md=12,style={'background-color': 'white'}),
+        dbc.Col(Izquierda, md=6,style={'background-color': 'blue'}),
+        dbc.Col(Derecha, md=6,style={'background-color': 'red'})
     ])
 ])
 
--- app.py
+++ app.py
@@ -7,14 +7,13 @@ Daniel Baquero
 Johan Peñaloza
 '''
 import dash
-from dash import html 
-import dash_bootstrap_components as dbc  
-
-#from fronted.fronted import layout
+import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-#app.layout = layout
+from Frontend.frontend import layout
+
+app.layout = layout
 
 if __name__ =='__main__':
     app.run_server(debug=True)
