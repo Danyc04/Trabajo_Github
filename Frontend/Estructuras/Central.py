b4610f4ba838e44d4d5075e34d4bd3212974440e
JohanPe65 <163649619+JohanPe65@users.noreply.github.com>
2024-04-21 19:40:01 -0500

    central

--- Frontend/Estructuras/Central.py
+++ Frontend/Estructuras/Central.py
@@ -3,13 +3,20 @@ import dash_bootstrap_components as dbc
 
 global central_arriba, central_abajo
 central_arriba = dbc.Container([
-    dbc.Row([
-        dbc.Col("Origen", md=2, style={'border': f'1px solid darkmagenta', 'color':'darkmagenta'}),
-        dbc.Col("Destino", md=2, style={'border': f'1px solid darkmagenta', 'color':'darkmagenta'}),
-        dbc.Col("Fecha ida (y regreso)", md=4, style={'border': f'1px solid darkmagenta', 'color':'darkmagenta'}),
-        dbc.Col("Cantidad personas", md=2, style={'border': f'1px solid darkmagenta', 'color':'darkmagenta'}),
+    html.H5(dbc.Row([
+        dbc.Col(dcc.Dropdown(
+            ['Bogota'], value='origen'), md=2, style={'border': f'1px solid darkmagenta', 'color':'darkmagenta'}),
+            dbc.Col(dcc.Dropdown(
+            ['Cartagena','Medellin','Barranquilla','Cali','Bucaramanga','Santa Marta'],
+            value= 'Destino'),
+            md=2, style={'border': f'1px solid darkmagenta', 'color':'darkmagenta'}),
+        dbc.Col(dcc.RadioItems(['Temp alta', 'Temp baja']), md=4, style={'border': f'1px solid darkmagenta', 'color':'darkmagenta'}),
+        dbc.Col(dcc.Dropdown(
+            ['1','2','3','4','5'],
+            value = "Personas"),
+            md=2, style={'border': f'1px solid darkmagenta', 'color':'darkmagenta'}),
         dbc.Col("Buscar", md=2, style={'border': f'1px solid darkmagenta', 'color':'darkmagenta'})
-        ]),
+        ])),
     dbc.Row([
         dbc.Col(dcc.Checklist(["Botón ida y regreso"]), md=12)
         ])
