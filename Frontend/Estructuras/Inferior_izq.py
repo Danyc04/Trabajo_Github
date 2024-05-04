58d849bb04598b0319cacd18748b4e4c0ef98172
Danyc04 <161129732+Danyc04@users.noreply.github.com>
2024-05-04 10:11:47 -0500

    Update Inferior_izq.py

--- Frontend/Estructuras/Inferior_izq.py
+++ Frontend/Estructuras/Inferior_izq.py
@@ -4,12 +4,12 @@ import base64
 
 global oferta_izq
 
-# with open('bd\imagen 1.jpg', 'rb') as img_file:
-    # encoded_img = base64.b64encode(img_file.read()).decode('ascii')
+with open('bd/imagen 1.jpg', 'rb') as img_file:
+    encoded_img = base64.b64encode(img_file.read()).decode('ascii')
 
-oferta_izq = html.Div([
+oferta_der = html.Div([
     html.H3("Aprovecha esta increíble oferta", style={'color':'indigo', 'textAlign': 'center'}),
-    # html.Img(src='https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/kkconcertkol.jpg?VersionId=X69XgQi6xidSzzk93srjDt2DrVFJxghU;base64,{encoded_img}',
-             # style={'width': '70%', 'height': 'auto', 'display': 'block', 'margin': 'auto'}),
+    html.Img(src=f'data:image/png;base64,{encoded_img}',
+             style={'width': '70%', 'height': 'auto', 'display': 'block', 'margin': 'auto'}),
     html.P("Dale click para mayor información", style={'textAlign': 'center'})
 ])
