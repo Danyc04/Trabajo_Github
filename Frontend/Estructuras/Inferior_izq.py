596237681e99a272f7ccb49491cedea02fd1d67a
Danyc04 <161129732+Danyc04@users.noreply.github.com>
2024-05-04 10:19:28 -0500

    Update Inferior_izq2.py

--- Frontend/Estructuras/Inferior_izq.py
+++ Frontend/Estructuras/Inferior_izq.py
@@ -7,7 +7,7 @@ global oferta_izq
 with open('bd/imagen 1.jpg', 'rb') as img_file:
     encoded_img = base64.b64encode(img_file.read()).decode('ascii')
 
-oferta_der = html.Div([
+oferta_izq = html.Div([
     html.H3("Aprovecha esta increíble oferta", style={'color':'indigo', 'textAlign': 'center'}),
     html.Img(src=f'data:image/png;base64,{encoded_img}',
              style={'width': '70%', 'height': 'auto', 'display': 'block', 'margin': 'auto'}),
